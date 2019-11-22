b55e4fde7d53019a8a0eeddf6090fddd298bad62
saebomi <saebomi0324@naver.com>
2019-11-22 17:41:03 +0900

    TodoHead 수정

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useTodoState } from '../TodoContext';
 
 const TodoHeadBlock = styled.div`
   padding-top: 48px;
@@ -26,13 +27,15 @@ const TodoHeadBlock = styled.div`
 `;
 
 function TodoHead() {
-    return (
-      <TodoHeadBlock>
-        <h1>2019년 11월 19일</h1>
-        <div className="day">수요일</div>
-        <div className="tasks-left">할 일 2개 남음</div>
-      </TodoHeadBlock>
-    );
-  }
-  
-  export default TodoHead;
+  const todos = useTodoState();
+  console.log(todos);
+  return (
+    <TodoHeadBlock>
+      <h1>2019년 7월 10일</h1>
+      <div className="day">수요일</div>
+      <div className="tasks-left">할 일 2개 남음</div>
+    </TodoHeadBlock>
+  );
+}
+
+export default TodoHead;
