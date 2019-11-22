d0b7ec6675189501d377365404ebd2dd0eca1346
saebomi <saebomi0324@naver.com>
2019-11-22 17:39:24 +0900

    TodoProvider 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
 import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
 
 const GlobalStyle = createGlobalStyle`
-body {
-  background: #e9ecef;
-}
+  body {
+    background: #e9ecef;
+  }
 `;
 
 function App() {
   return (
-    <>
-    <GlobalStyle />
-    <TodoTemplate>
-      <TodoHead />
-      <TodoList />
-      <TodoCreate />
-    </TodoTemplate>
-    </>
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
   );
 }
 
 export default App;
+
+
+
+
+
